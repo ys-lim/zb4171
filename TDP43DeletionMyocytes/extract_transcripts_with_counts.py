4b18ad2cf838d0c81c7f0d871e196ad55e92bc6f
ys-lim <68455070+ys-lim@users.noreply.github.com>
2021-11-02 09:33:51 +0800

    python script for extracting transcripts from quant.sf file

---
+++ TDP43DeletionMyocytes/extract_transcripts_with_counts.py
@@ -0,0 +1,33 @@
+# Usage: python3 extract_transcripts_with_counts.py ./quant.sf ./outputTPM ./outputCounts
+
+import sys
+import os
+
+inFile = sys.argv[1]
+outFile1 = sys.argv[2]
+outFile2 = sys.argv[3]
+
+temp = open("temp","w")
+output1 = open(outFile1, "w")
+output2 = open(outFile2, "w")
+
+with open(inFile, "r") as input:
+	next(input)
+	for entry in input:
+		entry = entry.split("\t")
+		if float(entry[3]) > 0 or float(entry[4]) > 0:
+			temp.write("\t".join(entry))
+			
+temp.close()
+
+lines = open("temp", 'r').readlines()
+
+def sort_transcripts(col, transcripts, file):
+	file.write("Name"+"\t"+"Length"+"\t"+"EffectiveLength"+"\t"+"TPM"+"\t"+"NumReads"+"\n")
+	for line in sorted(transcripts, key = lambda line: float(line.split("\t")[col]), reverse=True):
+		file.write(line)
+	file.close()
+
+sort_transcripts(3, lines, output1)
+sort_transcripts(4, lines, output2)
+os.system("rm temp")

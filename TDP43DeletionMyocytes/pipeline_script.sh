27aca810c3c2becc6649b01b82f919b52426711a
ys-lim <68455070+ys-lim@users.noreply.github.com>
2021-11-02 09:32:27 +0800

    Update pipeline_script.sh

--- TDP43DeletionMyocytes/pipeline_script.sh
+++ TDP43DeletionMyocytes/pipeline_script.sh
@@ -129,3 +129,31 @@ samtools collate --threads 8 -u -O /mnt/gtklab01/yongshan/project/12oct_jeong/re
 
 # knockout 2
 ~/STAR-2.7.9a/bin/Linux_x86_64/STAR --runThreadN 12 --genomeDir /mnt/gtklab01/linglab/mmusculus_annotation_files/STARv2.7.9a_index_mmusculus_gencode.vM27 --readFilesIn /mnt/gtklab01/yongshan/project/12oct_jeong/results/SRR5074121/SRR5074121_unmapped_pair1.fq.gz /mnt/gtklab01/yongshan/project/12oct_jeong/results/SRR5074121/SRR5074121_unmapped_pair2.fq.gz --readFilesCommand zcat --outFileNamePrefix /mnt/gtklab01/yongshan/project/12oct_jeong/results/SRR5074121/SRR5074121_cko1_unmapped_aligned_ --outSAMtype BAM SortedByCoordinate --outSAMattributes Standard --outSAMunmapped Within
+
+# check library preparation strand
+# https://github.com/betsig/how_are_we_stranded_here
+check_strandedness --gtf /mnt/gtklab01/linglab/mmusculus_annotation_files/gencode.vM27.primary_assembly.annotation.gtf --transcripts /mnt/gtklab01/linglab/mmusculus_annotation_files/gencode.vM27.transcripts.fa --reads_1 /mnt/gtklab01/yongshan/project/12oct_jeong/SRR5074051/SRR5074051_1.fastq.gz --reads_2 /mnt/gtklab01/yongshan/project/12oct_jeong/SRR5074051/SRR5074051_2.fastq.gz
+
+checking strandedness
+Reading reference gene model stranded_test_SRR5074051_1/gencode.vM27.primary_assembly.annotation.bed ... Done
+Loading SAM/BAM file ...  Total 200000 usable reads were sampled
+This is PairEnd Data
+Fraction of reads failed to determine: 0.1411
+Fraction of reads explained by "1++,1--,2+-,2-+": 0.4267 (49.7% of explainable reads)
+Fraction of reads explained by "1+-,1-+,2++,2--": 0.4321 (50.3% of explainable reads)
+Under 60% of reads explained by one direction
+Data is likely unstranded
+
+# sailfish quant
+# possible unstranded protocols: IU, OU, MU (https://sailfish.readthedocs.io/_/downloads/en/develop/pdf/)
+
+/mnt/gtklab01/yongshan/project/SailfishBeta-0.10.0_CentOS5/bin/sailfish quant -l IU -i /mnt/gtklab01/yongshan/project/21sept_scn/sailfish_idx -1 <(gunzip -c /mnt/gtklab01/yongshan/project/12oct_jeong/results/SRR5074051/SRR5074051_unmapped_pair1.fq.gz) -2 <(gunzip -c /mnt/gtklab01/yongshan/project/12oct_jeong/results/SRR5074051/SRR5074051_unmapped_pair2.fq.gz) -o /mnt/gtklab01/yongshan/project/12oct_jeong/results/sailfish_out/SRR5074051
+
+/mnt/gtklab01/yongshan/project/SailfishBeta-0.10.0_CentOS5/bin/sailfish quant -l IU -i /mnt/gtklab01/yongshan/project/21sept_scn/sailfish_idx -1 <(gunzip -c /mnt/gtklab01/yongshan/project/12oct_jeong/results/SRR5074077/SRR5074077_unmapped_pair1.fq.gz) -2 <(gunzip -c /mnt/gtklab01/yongshan/project/12oct_jeong/results/SRR5074077/SRR5074077_unmapped_pair2.fq.gz) -o /mnt/gtklab01/yongshan/project/12oct_jeong/results/sailfish_out/SRR5074077
+
+/mnt/gtklab01/yongshan/project/SailfishBeta-0.10.0_CentOS5/bin/sailfish quant -l IU -i /mnt/gtklab01/yongshan/project/21sept_scn/sailfish_idx -1 <(gunzip -c /mnt/gtklab01/yongshan/project/12oct_jeong/results/SRR5074121/SRR5074121_unmapped_pair1.fq.gz) -2 <(gunzip -c /mnt/gtklab01/yongshan/project/12oct_jeong/results/SRR5074121/SRR5074121_unmapped_pair2.fq.gz) -o /mnt/gtklab01/yongshan/project/12oct_jeong/results/sailfish_out/SRR5074121
+
+/mnt/gtklab01/yongshan/project/SailfishBeta-0.10.0_CentOS5/bin/sailfish quant -l IU -i /mnt/gtklab01/yongshan/project/21sept_scn/sailfish_idx -1 <(gunzip -c /mnt/gtklab01/yongshan/project/12oct_jeong/results/SRR5074186/SRR5074186_unmapped_pair1.fq.gz) -2 <(gunzip -c /mnt/gtklab01/yongshan/project/12oct_jeong/results/SRR5074186/SRR5074186_unmapped_pair2.fq.gz) -o /mnt/gtklab01/yongshan/project/12oct_jeong/results/sailfish_out/SRR5074186
+
+# run python script (extract_transcripts_with_counts.py) that takes in a quant.sf file and extracts all transcripts with counts > 0, then sorts them in descending order according to TPM or Readcounts
+python3 extract_transcripts_with_counts.py ./quant.sf ./outputTPM ./outputCounts
